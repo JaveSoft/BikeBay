2284dc308b621a45e3654672e195a3cb47e04930
David Gutiérrez <richiegut30@gmail.com>
2024-05-31 02:52:55 +0000

    Actualización No. 11

---
+++ bikebay/src/main/java/com/bicicleta/bikebay/BikebayApplication.java
@@ -0,0 +1,13 @@
+package com.bicicleta.bikebay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikebayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikebayApplication.class, args);
+	}
+
+}
