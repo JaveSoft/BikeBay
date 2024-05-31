4988607f7bed52b8a6a91a7072e46a9d047e89e1
David Gutiérrez <richiegut30@gmail.com>
2024-05-31 02:53:18 +0000

    Actualización No. 13

---
+++ bikebay/src/test/java/com/bicicleta/bikebay/BikebayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicicleta.bikebay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikebayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
